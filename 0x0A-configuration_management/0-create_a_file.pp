778114f35ffb3ca9259daccb94890d66574b6ea3
Ovakoma <ideleovercomer18@gmail.com>
2023-03-25 00:59:19 +0000

    Updated 0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 #Manifest contains declaration of a file resource
 file { '/tmp/school':
-  ensure    => 'file',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
 #Manifest kills a process using pkill command
-exec { "pkill":
-  command  => 'pkill -f killmenow',
-  path     => 'usr/bin/:/usr/local/bin/:/bin/',
+exec { "pkill killmenow":
+  path     => '/usr/bin:/usr/sbin:/bin',
 }
